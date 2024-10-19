faf3c4beb30f7ada1bec2ac61d267ec49a66d376
irashovdra <109846925+irashovdra@users.noreply.github.com>
2024-10-18 20:14:27 -0500

    Updates

---
+++ index.1b774b90.js
@@ -0,0 +1,2 @@
+const t=document.querySelector(".tickets__list");fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=Jg7o3tD8hioRQqQCvzEmn8hsX9ftNiZq").then(t=>t.json()).then(e=>{t.innerHTML=e});
+//# sourceMappingURL=index.1b774b90.js.map
